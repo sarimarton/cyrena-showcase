e40867fb2f26503fc55d87adfa37720356e35433
sarimarton <sarimarton@gmail.com>
2019-02-28 02:34:16 +0100

    Cleanup

--- src/vdom-stream-component.js
+++ src/vdom-stream-component.js
@@ -6,21 +6,25 @@ import mapValues from 'lodash/fp/mapValues'
 import castArray from 'lodash/castArray'
 import mergeWith from 'lodash/mergeWith'
 import compact from 'lodash/compact'
+import pick from 'lodash/pick'
 import omit from 'lodash/omit'
 
 const isComponent = node =>
   typeof node.type === 'function'
 
-const traverseVdom = action => (node, path = []) => {
-  if (action(node, path)) {
-    ;[node.props && node.props.children]
-      .flat()
-      .filter(_ => _)
-      .forEach((n, idx) => traverseVdom(action)(n, [...path, { node, idx }]))
+const traverseVdom = traverseAction => (node, path = [], acc = []) => {
+  if (traverseAction(node, path, acc)) {
+    compact(castArray(node.props && node.props.children))
+      .forEach((n, idx) => {
+        traverseVdom(traverseAction)(n, [...path, { node, idx }], acc)
+      })
   }
+
+  return acc
 }
 
 export const component = (sources, vdom, config) => {
+  // Wrap the whole tree in an additional root node to
   let hiddenRoot = {
     props: { children: [cloneDeep(vdom)] }
   }
@@ -28,40 +32,32 @@ export const component = (sources, vdom, config) => {
   const vdomProp = config.vdomProp
   const additionalTraverseAction = config.additionalTraverseAction || (() => {})
 
-  const cmps = []
-
-  const traverseAction = (node, path) => {
+  const traverseAction = (node, path, acc) => {
     const parent = path[path.length - 1] || { node: undefined, idx: 0 }
     const _isComponent = isComponent(node)
 
-    if (_isComponent) {
-      cmps.push({ node, parent })
-    }
-
     additionalTraverseAction(node, path)
+
+    if (_isComponent) {
+      acc.push({
+        node,
+        parent,
+        // Invoke cycle components in the vdom, and get the sinks
+        // Also pass key and props to them
+        sinks: node.type({ sources, ...pick(node, ['key', 'props']) })
+      })
+    }
 
     return !_isComponent
   }
 
-  traverseVdom(traverseAction)(hiddenRoot)
-
-  // Invoke cycle components in the vdom, and get the sinks
-  // Also pass key and props to them
-  let sinks = cmps.map(cfg =>
-    cfg.node.type({
-      sources,
-      key: cfg.node.key,
-      props: cfg.node.props
-    })
-  )
+  const cmps = traverseVdom(traverseAction)(hiddenRoot)
 
   // Get the vdoms from among the sinks
-  let vdoms = sinks.map(sink =>
-    sink[vdomProp]
-  )
+  const vdoms = cmps.map(cmp => cmp.sinks[vdomProp])
 
   // Combine the vdom streams and map them placed into the original structure
-  let vdom$ = xs.combine.apply(xs, vdoms)
+  const vdom$ = xs.combine.apply(xs, vdoms)
     .map(vdoms => {
       zip(vdoms, cmps).forEach(([vdom, cmp]) => {
         const idx = cmp.parent.idx
@@ -72,10 +68,7 @@ export const component = (sources, vdom, config) => {
             ? value => { props.children[idx] = value }
             : value => { props.children = value }
 
-        setter({
-          ...vdom,
-          key: vdom.key || originalNode.key
-        })
+        setter({ ...vdom, key: vdom.key || originalNode.key })
       })
 
       // Without cloning, React won't pick up the changes and the view
@@ -83,18 +76,21 @@ export const component = (sources, vdom, config) => {
       return cloneDeep(hiddenRoot.props.children[0])
     })
 
+  const sinks = cmps.map(cmp => cmp.sinks)
+
   // Gather all the other sinks which is not the vdom and merge them together
   // by type
-  let allOtherSinksOfAllComponents =
-    sinks.reduce(
-      (acc, next) => mergeWith(
-        acc,
-        omit(next, vdomProp),
-        (addition, src) => compact([...castArray(addition), src])
-      ),
-      mapValues(castArray)(config.otherSinks || {})
+  const allOtherSinksOfAllComponents =
+    mapValues(sinks => xs.merge.apply(xs, sinks))(
+      sinks.reduce(
+        (acc, next) => mergeWith(
+          acc,
+          omit(next, vdomProp),
+          (addition, src) => compact([...castArray(addition), src])
+        ),
+        mapValues(castArray)(config.otherSinks || {})
+      )
     )
-    |> mapValues(sinks => xs.merge.apply(xs, sinks))
 
   return {
     [vdomProp]: vdom$,
@@ -108,7 +104,7 @@ export const cycleReactComponent = (sources, vdom, otherSinks) => {
 
   const _config = {
 
-    // The vdom sink
+    // The name of the vdom sink
     vdomProp: 'react',
 
     // Prevent React warnings about lacking 'key' prop
