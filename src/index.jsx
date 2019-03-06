dd0f4bb6ffb8dfc64beac2c6ec4ec74b7f5a02e9
sarimarton <sarimarton@gmail.com>
2019-03-06 11:39:45 +0100

    Update showcase

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,27 @@
 import xs from 'xstream'
 import { run } from '@cycle/run'
 import { makeDOMDriver } from '@cycle/react-dom'
-import { makeHTTPDriver } from '@cycle/http'
 import { withState } from '@cycle/state'
 import { Fragment, useState } from 'react'
+
+import './style.css'
 
 import {
   pragma,
   withPower,
   ReactRealm,
   Scope,
-  useCycleState
+  useCycleState,
+  Collection,
+  get,
+  map,
+  component
 } from 'powercycle'
 
 /** @jsx pragma */
 /** @jsxFrag Fragment */
 
-function ReactComponent (props, state) {
+function ReactCounter (props, state) {
   const [count, setCount] = useState(0)
 
   return (
@@ -27,19 +32,18 @@ function ReactComponent (props, state) {
   )
 }
 
-function ReactComponentWithCycleState (props) {
+function ReactComboboxWithCycleState (props) {
   const [state, setState] = useCycleState(props.sources)
 
   return (
     <>
-      <div>Color:</div>
-      <select value={state.comboValue} onChange={e => { setState({ comboValue: e.target.value }) }}>
+      <label>Color: </label>
+      <select value={state.color} onChange={e => { setState({ ...state, color: e.target.value }) }}>
         <option value='#1e87f0'>default</option>
         <option value='red'>red</option>
-        <option value='purple'>puple</option>
+        <option value='purple'>purple</option>
         <option value='green'>green</option>
       </select>
-      <div>state: {JSON.stringify(state)}</div>
     </>
   )
 }
@@ -49,40 +53,33 @@ function ReactComponentWithCycleStateAndLens (props) {
 
   return (
     <>
-      <div>Color:</div>
+      <label>Color: </label>
       <select value={state} onChange={e => { setState(e.target.value) }}>
         <option value='#1e87f0'>default</option>
         <option value='red'>red</option>
-        <option value='purple'>puple</option>
+        <option value='purple'>purple</option>
         <option value='green'>green</option>
       </select>
-      <div>state: {JSON.stringify(state)}</div>
     </>
   )
 }
 
-function Timer () {
-  return (
-    <div>{xs.periodic(1000).startWith(-1)}</div>
-  )
-}
-
 function Counter (sources) {
-  const inc = Symbol('inc')
+  const inc = Symbol(0)
   const inc$ = sources.react.select(inc).events('click')
 
   const count$ = inc$.fold(count => count + 1, 0)
 
   return (
     <>
+      {count$}<br />
       <button sel={inc}>Incremenet</button>
-      <div>{count$}</div>
     </>
   )
 }
 
 function Combobox (sources) {
-  const select = Symbol('select')
+  const select = Symbol(0)
   const state$ = sources.state.stream
 
   const reducer$ =
@@ -90,33 +87,26 @@ function Combobox (sources) {
       .select(select)
       .events('change')
       .map(event => event.target.value)
-      .map(value => prevState => ({ ...prevState, comboValue: value }))
+      .map(value => prevState => ({ ...prevState, color: value }))
 
-  const comboValue$ = state$.map(s => s.comboValue)
+  const color$ = state$.map(s => s.color)
 
   return [
     <>
-      <div>Color:</div>
-      <select sel={select} defaultValue={comboValue$}>
+      <label>Color: </label>
+      <select sel={select} defaultValue={color$}>
         <option value='#1e87f0'>default</option>
         <option value='red'>red</option>
-        <option value='purple'>puple</option>
+        <option value='purple'>purple</option>
         <option value='green'>green</option>
       </select>
-      <div>state: {'{'} comboValue: {comboValue$} {'}'}</div>
     </>,
-    {
-      state: reducer$,
-      HTTP: xs.of({
-        url: '?example-http-request',
-        category: 'search'
-      })
-    }
+    { state: reducer$ }
   ]
 }
 
 function ComboboxWithLens (sources) {
-  const select = Symbol('select')
+  const select = Symbol(0)
   const state$ = sources.state.stream
 
   const reducer$ =
@@ -128,14 +118,13 @@ function ComboboxWithLens (sources) {
 
   return [
     <>
-      <div>Color:</div>
+      <label>Color: </label>
       <select sel={select} defaultValue={state$}>
         <option value='#1e87f0'>default</option>
         <option value='red'>red</option>
-        <option value='purple'>puple</option>
+        <option value='purple'>purple</option>
         <option value='green'>green</option>
       </select>
-      <div>state: "{state$}"</div>
     </>,
     { state: reducer$ }
   ]
@@ -144,21 +133,42 @@ function ComboboxWithLens (sources) {
 function Card (sources) {
   return (
     <div
-      className='uk-margin-right uk-width-1-4 uk-padding-small uk-card uk-card-default uk-card-body uk-card-primary'
-      style={{ ...sources.props.style, minWidth: 180, maxWidth: 300 }}
+      className='uk-card uk-card-default uk-padding-small uk-card-primary'
+      style={{ ...sources.props.style }}
     >
       {sources.props.title &&
-        <h3 className='uk-card-title'>{sources.props.title}</h3>}
+        <h5>{sources.props.title}</h5>}
       <div>{sources.props.children}</div>
     </div>
   )
 }
 
 function ShowState (sources) {
+  // return {
+  //   react: sources.state.stream.map(state => <Code>{JSON.stringify(state)}</Code>)
+  // }
+  //
+  // return component(
+  //   <Code>{sources.state.stream.map(state => JSON.stringify(state))}</Code>,
+  //   { /* event sinks */ },
+  //   sources
+  // )
+  //
+  // return [
+  //   <Code>{sources.state.stream.map(state => JSON.stringify(state))}</Code>,
+  //   { /* other sinks */ }
+  // ]
+  //
+  // return (
+  //   <Code>{sources.state.stream.map(state => JSON.stringify(state))}</Code>
+  // )
+  //
+  // return (
+  //   <Code>{map(JSON.stringify, sources)}</Code>
+  // )
+
   return (
-    <Code>
-      state: {sources.state.stream.map(state => JSON.stringify(state))}
-    </Code>
+    <Code>{map(JSON.stringify)}</Code>
   )
 }
 
@@ -170,66 +180,170 @@ function Code (sources) {
   )
 }
 
+function CollectionDemo (sources) {
+  const add = Symbol(0)
+
+  const add$ = sources.react.select(add).events('click')
+    .map(event => prevState => ([...prevState, { color: '#1e87f0' }]))
+
+  return [
+    <>
+      <div>
+        <button sel={add}>Add</button>
+      </div>
+      <br />
+      <div>
+        <Collection indexKey='idx'>
+          <pre>
+            {/* Different ways to get state key */}
+            (
+            {src => <>{src.state.stream.map(s => s.idx)}</>}
+            ,
+            {map(s => s.idx)}
+            ,
+            {get('idx')}
+            ,
+            <Scope lens='idx'>{get()}</Scope>
+            )&nbsp;
+
+            <Combobox />
+
+            {src => {
+              const remove = Symbol(0)
+
+              return [
+                <button sel={remove} style={{ float: 'right' }}>Remove</button>,
+                { state: src.react.select(remove).events('click')
+                  .map(event => prevState => undefined) }
+                // soon:
+                // { state: src[remove].click.map(event => state => undefined) }
+              ]
+            }}
+
+            <br />
+
+            {src =>
+              <div style={{ color: get('color', src) }}>
+                <ShowState />
+              </div>
+            }
+          </pre>
+        </Collection>
+      </div>
+    </>,
+    { state: add$ }
+  ]
+}
+
+function TodoList (sources) {
+  const add = Symbol(0)
+
+  const add$ = sources.react.select(add).events('click')
+    .map(event => prevState => [...prevState, { text: '' }])
+
+  const reducer$ = add$
+
+  return [
+    <>
+      <input /><button sel={add}>Add</button>
+      <Collection indexKey='idx'>
+        <div>
+          {sources => {
+            const remove = Symbol(0)
+            const input = Symbol(0)
+
+            const input$ = sources.react.select(input).events('change')
+              .map(event => event.target.value)
+              .map(value => prevState => ({ text: value }))
+
+            const remove$ = sources.react.select(remove).events('click')
+              .map(event => prevState => undefined)
+
+            return [
+              <>
+                <input sel={input} defaultValue={get('text', sources)} />
+                <button sel={remove}>Remove</button>
+              </>,
+              { state: xs.merge(input$, remove$) }
+            ]
+          }}
+        </div>
+      </Collection>
+    </>,
+    { state: reducer$ }
+  ]
+}
+
 function main (sources) {
   const state$ = sources.state.stream
 
-  const reducer$ = xs.of(() => ({ comboValue: 'red' }))
+  const reducer$ = xs.of(() => ({
+    color: 'red',
+    list: [{ color: 'red' }, { color: 'green' }],
+    todoList: [{ text: 'todo1' }, { text: 'todo2' }, { text: 'todo3' }],
+    foo: { bar: { baz: 5 } }
+  }))
 
-  const color$ = state$.map(state => state.comboValue)
+  const color$ = state$.map(state => state.color)
 
   return [
     <div className='uk-padding-small'>
-      <div className='uk-flex uk-margin'>
+      <h2>Powercycle Showcase</h2>
 
+      <div className='grid'>
         <Card title='Cycle JS component'>
           <Combobox />
         </Card>
 
-        <Card title='React realm'>
+        <Card title='React components under <ReactRealm>'>
           <ReactRealm>
-            <ReactComponent style={{ width: '90px', float: 'left' }} />
-            <ReactComponent style={{ width: '90px', float: 'left' }} />
+            <ReactCounter style={{ width: '90px', float: 'left' }} />
+            <ReactCounter style={{ width: '90px', float: 'left' }} />
             text node
           </ReactRealm>
         </Card>
 
-        <Card title='React cmp w/ Cycle state'>
+        <Card title='React component + Cycle state'>
           <ReactRealm>
-            <ReactComponentWithCycleState />
+            <ReactComboboxWithCycleState />
           </ReactRealm>
         </Card>
 
-        <Card title='Timer'>
-          <Timer />
+        <Card title='Little stuff'>
+          Timer: {xs.periodic(1000).startWith(-1)}
+          <br />
+          Counter: <Counter />
         </Card>
-
-      </div>
-      <div className='uk-flex uk-margin'>
 
         <Card title='Get state in nested component'>
           <ShowState />
         </Card>
 
-        <Card title='Counter'>
+        <Card title='Todo List'>
+          <TodoList lens='todoList' />
+        </Card>
+
+        <Card title='Another counter' style={{ display: 'none' }}>
           <Counter />
         </Card>
 
-        <Card title='Another counter'>
-          <Counter />
-        </Card>
+        <Card title='Simple input' lens='color'>
+          Color:
+          {sources => {
+            const input = Symbol(0)
 
-        <Card title='Stream text node'>
-          Combobox value:&nbsp;
-          {color$}
+            const input$ = sources.react.select(input).events('change')
+              .map(event => event.target.value)
+              .map(value => prevState => value)
+
+            return [
+              <input sel={input} value={get('', sources)} />,
+              { state: input$ }
+            ]
+          }}
           <br />
-          <Code>
-            {'<div>{color$}</div>'}
-          </Code>
         </Card>
 
-      </div>
-
-      <div className='uk-flex uk-margin'>
         <Card title={color$.map(color => `Stream travelling through prop: ${color}`)} />
 
         <Card title='Stream DOM prop' style={{ background: color$ }}>
@@ -238,27 +352,29 @@ function main (sources) {
           </Code>
         </Card>
 
-        <Scope lens='comboValue'>
-          <Card title='Lenses'>
-            <ComboboxWithLens />
-          </Card>
-        </Scope>
+        <Card title='Lenses'>
+          <ComboboxWithLens lens='color' />
+          <br />
+          foo.bar.baz: <ShowState lens='foo.bar.baz' />
+        </Card>
 
-        <Card title='React cmp w/ Lenses'>
-          <ReactRealm lens='comboValue'>
+        <Card title='React component + Cycle state + Lenses'>
+          <ReactRealm lens='color'>
             <ReactComponentWithCycleStateAndLens />
           </ReactRealm>
         </Card>
-      </div>
 
+        <Card title='Collection'>
+          <CollectionDemo lens='list' />
+        </Card>
+      </div>
     </div>,
     { state: reducer$ }
   ]
 }
 
 const drivers = {
-  react: makeDOMDriver(document.getElementById('app')),
-  HTTP: makeHTTPDriver()
+  react: makeDOMDriver(document.getElementById('app'))
 }
 
 run(withState(withPower(main)), drivers)
